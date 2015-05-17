f218a99403c8f041d22344d9b1a3b9c2b855e8b4
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-17 20:49:56 +0000

    add practices/section_1/practice_2/practice.js

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,26 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+
+  var collection_a = ["a", "e", "h", "t", "f", "c", "g", "b", "d"];
+var collection_b = [["a", "d", "e", "f"]];
+
+var result = new Array();
+
+for(var i=0;i<collection_a.length;i++){
+	for(var j=0;j<collection_b.length;j++){
+		for(var m=0;m<collection_b[j].length;m++){
+
+
+		if(collection_a[i]==collection_b[j][m]){
+
+			result.push(collection_a[i]);
+		}
+	}
+}
+}
+
+return result;
+
 }
 
 module.exports = collect_same_elements;
