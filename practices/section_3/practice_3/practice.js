92ca0f0943870c8c17018f7509aff851229e50ab
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-18 16:32:49 +0000

    add practices/section_3/practice_3/practice.js

--- practices/section_3/practice_3/practice.js
+++ practices/section_3/practice_3/practice.js
@@ -1,9 +1,10 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
   var array = [];
-  var exist = false;  // 定义标志：是否找到
+
 
   for(var i = 0; i < collection_a.length; i++){
+      var exist = false;  // 定义标志：是否找到
     for(var j = 0; j < array.length; j++){
         if(array[j].key == collection_a[i]){  // 判断是否找到
           array[j].count ++;  // 找到count++
@@ -16,7 +17,7 @@ function create_updated_collection(collection_a, object_b) {
         temp.count = 1;
         array.push(temp);
       }
-      exist = false;    // 默认找不到
+
     }
 
     for(var i  = 0; i < object_b.value.length; i++){
