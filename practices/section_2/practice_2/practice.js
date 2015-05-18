65a9a95b4a87a6aafcf14c195e9464d5a049d5c5
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-18 17:01:47 +0000

    add practices/section_2/practice_2/practice.js

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -2,26 +2,26 @@ function count_same_elements(collection) {
   //在这里写入代码
   var sum_list=[];
   for(var i=0;i<collection.length;i++) {
-          var summary=1;
-          var name=collection[i].substring(0,1);
-          if(collection[i].indexOf("-")>-1||collection[i].indexOf(":") >-1||collection[i].indexOf("[") >-1) {
-              summary=collection[i].substring(2);
-              summary=parseInt(summary);
-           }
-           var exsit=false;
-           for(var k=0;k<sum_list.length;k++) {
-               if(sum_list[k].name==name) {
-                        sum_list[k].summary=sum_list[k].summary+summary;
-                        exsit=true;
-               }
-           }
-           if(!exsit) {
-                  var temp={};
-                  temp.name=name;
-                  temp.summary=summary;
-                  sum_list.push(temp);
-           }
-      }
+           var count=1;
+           var key=collection[i].substring(0,1);
+           if(collection[i].indexOf("-")>-1) {
+               count=collection[i].substring(2);
+               count=parseInt(count);
+            }
+            var exsit=false;
+            for(var k=0;k<sum_list.length;k++) {
+                if(sum_list[k].key==key) {
+                         sum_list[k].count=sum_list[k].count+count;
+                         exsit=true;
+                }
+            }
+            if(!exsit) {
+                   var temp={};
+                   temp.key= key;
+                   temp.count= count;
+                   sum_list.push(temp);
+            }
+       }
       return sum_list;
 }
 
