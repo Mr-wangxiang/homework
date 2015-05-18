8fdd25ac3339e00028aeaab4713dc2ba420105e1
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-18 15:27:46 +0000

    add practices/section_2/practice_2/practice.js

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,24 +1,10 @@
 function count_same_elements(collection) {
   //在这里写入代码
-  vvar collection = [
-    "a", "a", "a",
-    "e", "e", "e", "e", "e", "e", "e",
-    "h", "h", "h", "h", "h", "h", "h", "h", "h", "h", "h",
-    "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t",
-    "f", "f", "f", "f", "f", "f", "f", "f", "f",
-    "c", "c", "c", "c", "c", "c", "c", "c",
-    "g", "g", "g", "g", "g", "g", "g",
-    "b", "b", "b", "b", "b", "b",
-    "d-5"
-  ];
-
-
-
-var sum_list=[];
+  var sum_list=[];
   for(var i=0;i<collection.length;i++) {
           var summary=1;
           var name=collection[i].substring(0,1);
-          if(collection[i].indexOf("-")>-1) {
+          if(collection[i].indexOf("-")>-1||collection[i].indexOf(":") >-1||collection[i].indexOf("[") >-1) {
               summary=collection[i].substring(2);
               summary=parseInt(summary);
            }
